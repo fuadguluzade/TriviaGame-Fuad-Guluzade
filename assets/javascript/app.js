a934c9e5ba9104deedee09c1d13a5c2d48e289ff
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-09-05 20:34:20 -0700

    Question appearing logic done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,26 @@
 var qArray = ["Which city is the capital of Russia?",
               "Which city is the capital of Germany?",
+              "Which city is the capital of Australia?",
+              "Which city is the capital of Thailand?",
+              "Which city is the capital of Nigeria?",
+              "Which city is the capital of Ecuador?",
+              "Which city is the capital of Honduras?",
+              "Which city is the capital of Tajikistan?",
+              "Which city is the capital of Switzerland?",
               "Which city is the capital of Azerbaijan?",
-              "Which city is the capital of Australia"];
+            ];
 var aArray = [["Kiev", "Moscow", "Minsk", "Belgrad"],
                 ["Munich", "Hamburg", "Bonn", "Berlin"],
-                ["Baku", "Tbilisi", "Yerevan", "Tehran"],
-                ["Melbourne", "Sidney", "Canberra", "Brisbane"]];
+                ["Melbourne", "Sidney", "Canberra", "Brisbane"],
+                ["Bangkok", "Hanoi", "Phnompenh", "Rangoon"],
+                ["Lagos", "Abuja", "Monrovia", "Brazzaville"],
+                ["Lima", "Quito", "Bogota", "Caracas"],
+                ["Managua", "San Jose", "Belmopan", "Tegucigalpa"],
+                ["Tashkent", "Ashgabat", "Dushanbe", "Bishkek"],
+                ["Geneva", "Zurich", "Lucerne", "Bern"],
+                ["Baku", "Tbilisi", "Yerevan", "Tehran"]];
 var qNumber = 0;
-var corAnsArray = [1, 3, 0, 2];
+var corAnsArray = [1, 3, 2, 0, 1, 1, 3, 2, 3, 0];
 
 
 $(document).ready(function () {
@@ -26,7 +39,7 @@ $(document).ready(function () {
 
 const showQuestion = function () {
     $("#question").text(qArray[qNumber]);
-    $("#ans").each(function (ansIndex) {
+    $("h5").children().each(function (ansIndex) {
         $(this).text(aArray[qNumber][ansIndex]);
     });
 };
