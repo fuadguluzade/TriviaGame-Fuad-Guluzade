f11dcc76da4c769ad5613c9c248c3c4a8d4922b4
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-09-06 23:13:37 -0700

    pre-final version done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,47 @@
 var qArray = ["Which city is the capital of Russia?",
-              "Which city is the capital of Germany?",
-              "Which city is the capital of Australia?",
-              "Which city is the capital of Thailand?",
-              "Which city is the capital of Nigeria?",
-              "Which city is the capital of Ecuador?",
-              "Which city is the capital of Honduras?",
-              "Which city is the capital of Tajikistan?",
-              "Which city is the capital of Switzerland?",
-              "Which city is the capital of Azerbaijan?",
-            ];
+    "Which city is the capital of Germany?",
+    "Which city is the capital of Australia?",
+    "Which city is the capital of Thailand?",
+    "Which city is the capital of Nigeria?",
+    "Which city is the capital of Ecuador?",
+    "Which city is the capital of Honduras?",
+    "Which city is the capital of Tajikistan?",
+    "Which city is the capital of Switzerland?",
+    "Which city is the capital of Azerbaijan?",];
+
 var aArray = [["Kiev", "Moscow", "Minsk", "Belgrad"],
-                ["Munich", "Hamburg", "Bonn", "Berlin"],
-                ["Melbourne", "Sidney", "Canberra", "Brisbane"],
-                ["Bangkok", "Hanoi", "Phnompenh", "Rangoon"],
-                ["Lagos", "Abuja", "Monrovia", "Brazzaville"],
-                ["Lima", "Quito", "Bogota", "Caracas"],
-                ["Managua", "San Jose", "Belmopan", "Tegucigalpa"],
-                ["Tashkent", "Ashgabat", "Dushanbe", "Bishkek"],
-                ["Geneva", "Zurich", "Lucerne", "Bern"],
-                ["Baku", "Tbilisi", "Yerevan", "Tehran"]];
+["Munich", "Hamburg", "Bonn", "Berlin"],
+["Melbourne", "Sidney", "Canberra", "Brisbane"],
+["Bangkok", "Hanoi", "Phnompenh", "Rangoon"],
+["Lagos", "Abuja", "Monrovia", "Brazzaville"],
+["Lima", "Quito", "Bogota", "Caracas"],
+["Managua", "San Jose", "Belmopan", "Tegucigalpa"],
+["Tashkent", "Ashgabat", "Dushanbe", "Bishkek"],
+["Geneva", "Zurich", "Lucerne", "Bern"],
+["Baku", "Tbilisi", "Yerevan", "Tehran"]];
 
 var imgArray = ["./assets/images/moscow.jpg",
-                "/assets/images/berlin.jpg",
-                "./assets/images/canberra.jpg",
-                "./assets/images/bangkok.jpeg",
-                "./assets/images/abuja.jpeg",
-                "./assets/images/quito.jpg",
-                "./assets/images/tegucigalpa.jpg",
-                "./assets/images/dushanbe.jpg",
-                "./assets/images/bern.png",
-                "./assets/images/Baku.png"]
-var qNumber = 0;
-var corAnsArray = [1, 3, 2, 0, 1, 1, 3, 2, 3, 0];
+    "./assets/images/berlin.jpg",
+    "./assets/images/canberra.jpg",
+    "./assets/images/bangkok.jpeg",
+    "./assets/images/abuja.jpeg",
+    "./assets/images/quito.jpg",
+    "./assets/images/tegucigalpa.jpg",
+    "./assets/images/dushanbe.jpg",
+    "./assets/images/bern.png",
+    "./assets/images/Baku.png"]
+
 $(".win-container").hide();
 $(".lose-container").hide();
 $(".oot-container").hide();
+$(".gameover-container").hide();
+
+
+var qNumber = 0;
+var corAnsArray = [1, 3, 2, 0, 1, 1, 3, 2, 3, 0];
 var correct = 0;
 var wrong = 0;
+var notAnswered = 0;
 var time = 30;
 
 $(document).ready(function () {
@@ -53,6 +58,19 @@ $(document).ready(function () {
             showLoseContainer();
         }
     });
+
+    $("#restart").on('click', function () {
+        $(".win-container").hide();
+        $(".lose-container").hide();
+        $(".oot-container").hide();
+        $(".gameover-container").hide();
+        qNumber = 0;
+        correct = 0;
+        wrong = 0;
+        notAnswered = 0;
+        showQuestion();
+        resetTimer();
+    });
 });
 
 
@@ -62,24 +80,52 @@ const showQuestion = function () {
     $(".ans").each(function (ansIndex) {
         $(this).text(aArray[qNumber][ansIndex]);
     });
+    $(".question-container").show();
 };
 
 const showNextQuestion = function () {
     qNumber++;
-    showQuestion();
+    if (qNumber === 10) {
+        stopTimer();
+        showGameoverContainer();
+    } else {
+        resetTimer();
+        showQuestion();
+    }
+
 }
 
 const showWinContainer = function () {
     $(".img").attr('src', imgArray[qNumber]);
     $(".question-container").hide();
     $(".win-container").show();
+    setTimeout(() => $(".win-container").hide(), 1000);
+    setTimeout(showNextQuestion, 1000);
 };
 
 const showLoseContainer = function () {
     $(".img").attr('src', imgArray[qNumber]);
-    $("#corAns").text(aArray[qNumber][corAnsArray[qNumber]]);
+    $(".corAns").text(aArray[qNumber][corAnsArray[qNumber]]);
     $(".question-container").hide();
     $(".lose-container").show();
+    setTimeout(() => $(".lose-container").hide(), 1000);
+    setTimeout(showNextQuestion, 1000);
+}
+
+const showOotContainer = function () {
+    $(".img").attr('src', imgArray[qNumber]);
+    $(".corAns").text(aArray[qNumber][corAnsArray[qNumber]]);
+    $(".question-container").hide();
+    $(".oot-container").show();
+    setTimeout(() => $(".oot-container").hide(), 1000);
+    setTimeout(showNextQuestion, 1000);
+}
+
+const showGameoverContainer = function () {
+    $("#correct").text(correct);
+    $("#wrong").text(wrong);
+    $("#notAnswered").text(notAnswered);
+    $(".gameover-container").show();
 }
 
 
@@ -87,19 +133,25 @@ const startTimer = function () {
     interval = setInterval(count, 1000);
 };
 
-const resetTimer = function() {
+const resetTimer = function () {
+    stopTimer();
+    time = 30;
     $("#time").text(30);
+    startTimer();
 };
 
-const stopTimer = function() {
+const stopTimer = function () {
     clearInterval(interval);
 };
 
-function count() {
+const count = function () {
     time--;
     $("#time").text(time);
-    if (time === 0)  
-        stopTimer();
+    if (time === 0) {
+        notAnswered++;
+        showOotContainer();
+    }
+
 };
 
 
